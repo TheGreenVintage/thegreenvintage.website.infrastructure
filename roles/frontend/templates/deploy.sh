ed86d3a951c6480f8417a4031cacb1eb764c932c
Ferran Basora <fcsonline@gmail.com>
2017-06-27 14:51:37 +0200

    Read env

--- roles/frontend/templates/deploy.sh
+++ roles/frontend/templates/deploy.sh
@@ -4,4 +4,4 @@ set -e
 
 GIT_BRANCH=$1
 
-exec sudo -u {{ user }} /bin/bash /home/{{ user }}/shared/deploy/static.sh "$GIT_BRANCH"
+exec sudo -i -u {{ user }} /bin/bash /home/{{ user }}/shared/deploy/static.sh "$GIT_BRANCH"
